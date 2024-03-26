89f68333e0a03be68fba6e6a50c410e9dbb1dfac
Deepak Chowdry <113930530+DeepakChowdry-bit@users.noreply.github.com>
2024-03-26 15:44:16 +0530

    Update Navbar.jsx
    Navbar bg fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-center h-16 fixed w-full bg-slate-900'>
+    <div className='flex items-center justify-center h-16 fixed w-full bg-slate-950'>
       <div className='flex items-center justify-center w-[85%]'>
         <h3 className='font-semibold text-base tracking-wider text-slate-100'>
           PASSMAN
