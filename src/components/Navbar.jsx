1f7fadd22cba6e053e97432f04a7dc0566307c07
Deepak Chowdry <113930530+DeepakChowdry-bit@users.noreply.github.com>
2024-03-26 15:42:56 +0530

    Update Navbar.jsx
    Navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-center h-16 fixed w-full bg-slate-100'>
+    <div className='flex items-center justify-center h-16 fixed w-full bg-slate-900'>
       <div className='flex items-center justify-center w-[85%]'>
-        <h3 className='font-semibold text-base tracking-wider text-slate-950'>
+        <h3 className='font-semibold text-base tracking-wider text-slate-100'>
           PASSMAN
         </h3>
       </div>
